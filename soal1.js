3ebfadd55d2d6f21971555e2fcc8408463709141
Husni-Mubarak <husnimubarak859@gmail.com>
2023-02-18 23:49:37 +0700

    Menambahkan console.log untuk masing-masing rumus

--- soal1.js
+++ soal1.js
@@ -2,4 +2,9 @@ const rumus = require("./rumus");
 
 console.log(rumus.luasPersegi(5), rumus.luasPersegiPanjang(2, 4), rumus.kelilingPersegi(5), rumus.kelilingPersegiPanjang(2, 4));
 
+console.log(`Luas Persegi Anda Sebesar ${rumus.luasPersegi(5)} m²`);
+console.log(`Luas Persegi-Panjang Anda Sebesar ${rumus.luasPersegiPanjang(2, 4)} m²`);
+console.log(`Panjang keliling Persegi Anda ${rumus.kelilingPersegi(5)} m'`);
+console.log(`Panjang keliling Persegi-Panjang Anda ${rumus.kelilingPersegiPanjang(2, 4)} m'`);
+
 //  https://github.com/Husni-Mubarak/homeworkWeek7.git
